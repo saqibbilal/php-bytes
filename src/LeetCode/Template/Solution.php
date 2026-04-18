9042732c57589fce62916db8c546dd3c50b8fb70
Saqib Bilal <saqib_bilal786@yahoo.com>
2026-04-18 03:26:20 -0400

    chore: add boilerplate template for new LeetCode challenges

---
+++ src/LeetCode/Template/Solution.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Saqib\PhpBytes\LeetCode\ProblemName;
+
+class Solution
+{
+    /**
+     * @param int[] $nums
+     * @param int $target
+     * @return int[]
+     */
+    public function solve(array $nums, int $target): array
+    {
+        // Your logic here...
+        return [];
+    }
+}
